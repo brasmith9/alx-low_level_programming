61fa3b6d0a88c1f2a3c0b0102472dcfde97e0a2e
isaac <isaacanane9@gmail.com>
2022-09-15 03:12:26 -0700

    infinte loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - causes an infintie loop
+* Return: 0
+**/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incomin:g :(\n");
+
+	i = 0;
+	/*
+	*while (i < 0)
+	*{
+	*	putchar(i);
+	*}
+	**/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
