d5e724f0c5fac3df88e7d8969649ecca326b41f6
isaac <isaacanane9@gmail.com>
2022-10-04 09:39:17 -0700

    print file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - main
+* @argc: argc
+* @argv: argv
+* Return: int
+*/
+
+int main(int argc, char *argv[])
+{
+	int i = argc - argc;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
