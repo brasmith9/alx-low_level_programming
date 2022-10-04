5f7048e8fcb36aa92dada7c832e9e6cad7c29ea6
isaac <isaacanane9@gmail.com>
2022-10-04 09:47:37 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - main
+* @argc: argc
+* @argv: argv
+* Return: int
+*/
+
+int main(int argc, char *argv[])
+{
+	int u = argc - 1;
+	(void)argv;
+	printf("d\n", u);
+}
+
